4871b19e2e99ae49003a7491e08ea0756aa0889b
Cory <coryDav.Dev@gmail.com>
2025-07-24 14:08:32 -0400

    idk why it didnt commit b4

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ echo "Installing backend dependencies..."
 cd backend && npm init -y || exit 1
 
 echo "Creating vite project"
-cd frontend && npm create vite@latest || exit 1
+cd ../frontend && npm create vite@latest || exit 1
 
 echo "Done ✅"
