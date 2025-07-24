99bdf191cc9de4ccf40d00c5df04282f9e174029
Cory <coryDav.Dev@gmail.com>
2025-07-24 14:07:30 -0400

    final final fix of install script lol

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 echo "Installing backend dependencies..."
-cd backend 
-npm init -y
+cd backend && npm init -y || exit 1
 
 echo "Creating vite project"
-cd frontend
-npm create vite@latest
-
+cd frontend && npm create vite@latest || exit 1
 
 echo "Done ✅"
